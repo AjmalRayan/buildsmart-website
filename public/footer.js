b14756d25b9421efcfb92b4220f37cb3052d1bf4
unknown <ajmalak016@gmail.com>
2025-03-06 20:10:27 +0530

    Update commit: all the pages are updated

---
+++ public/footer.js
@@ -0,0 +1,65 @@
+document.getElementById("footer-placeholder").innerHTML = `
+  <footer class="bg-dark text-white pt-5 pb-4">
+    <div class="container text-center text-md-left">
+      <div class="row text-center text-md-left">
+
+        <!-- About Company -->
+        <div class="col-md-3 mx-auto">
+          <h5 class="text-uppercase font-weight-bold">About Company</h5>
+          <p>SA CONSTRUCTION</p>
+          <p>⭐⭐⭐⭐⭐<br>Rated 5/5 based on 105 customer ratings</p>
+          <div>
+            <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+            <a href="#" class="text-white"><i class="fab fa-whatsapp"></i></a>
+          </div>
+        </div>
+
+        <!-- Quick Links -->
+        <div class="col-md-3 mx-auto">
+          <h5 class="text-uppercase font-weight-bold">Quick Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="index.html" class="text-white">Home</a></li>
+            <li><a href="about.html" class="text-white">About Us</a></li>
+            <li><a href="team.html" class="text-white">Our Team</a></li>
+            <li><a href="works.html" class="text-white">Our Works</a></li>
+            <li><a href="interior.html" class="text-white">Interior</a></li>
+            <li><a href="construction-package.html" class="text-white">Construction Package</a></li>
+            <li><a href="design-package.html" class="text-white">Design Package</a></li>
+            
+            
+            
+            
+          </ul>
+        </div>
+
+        <!-- Contact Info -->
+        <div class="col-md-3 mx-auto">
+          <h5 class="text-uppercase font-weight-bold">Contact Info</h5>
+          <p>No.140J, Vengatasalam Colony,<br>Pallivasal Street, Pattukkottai-614601</p>
+          <p><i class="fas fa-phone-alt"></i> +91 875 4709 319</p>
+          <p><i class="fas fa-envelope"></i> ajmalak016@gmail.com</p>
+          <p><i class="fas fa-globe"></i> www.saconstruction.com</p>
+        </div>
+
+        <!-- SITE Locations -->
+        <div class="col-md-3 mx-auto">
+          <h5 class="text-uppercase font-weight-bold">Office Locations</h5>
+          <p><i class="fas fa-map-marker-alt"></i> Pattukkottai</p>
+          <p><i class="fas fa-map-marker-alt"></i> Adirampattinam</p>
+          <p><i class="fas fa-map-marker-alt"></i> Madukkur</p>
+          <p><i class="fas fa-map-marker-alt"></i> Manargudi</p>
+          <p><i class="fas fa-map-marker-alt"></i> Peravurani</p>
+          <p><i class="fas fa-map-marker-alt"></i> Tanjore</p>
+        </div>
+
+      </div>
+      <hr class="mb-4">
+      <div class="row">
+        <div class="col text-center">
+          <p>Copyright © 2025 SA CONSTRUCTION. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+`;
