b93b6c5f6e5de69ee635ad012d320de9501a7bb3
unknown <ajmalak016@gmail.com>
2025-03-09 14:57:15 +0530

    Fixed email sending issue

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/send-email', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5004;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
